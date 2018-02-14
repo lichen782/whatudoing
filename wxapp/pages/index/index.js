7117d7b7956672fe6cb61637cbd38010ae15d990
Chen Li <lichen782@gmail.com>
2018-02-14 17:41:40 +0800

    Integrate a calendar UI on front page.

--- wxapp/pages/index/index.js
+++ wxapp/pages/index/index.js
@@ -1,8 +1,9 @@
 //index.js
 //获取应用实例
+var util = require('../../utils/util.js');
 var d = new Date()
 const app = getApp()
-const today = d.getFullYear() + '年' + (d.getMonth() + 1) + '月' + d.getDate() + '日'
+const todayStr = d.getFullYear() + '年' + (d.getMonth() + 1) + '月' + d.getDate() + '日'
 
 Page({
   data: {
@@ -13,13 +14,9 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     currentTab: 0,
-    currentDate: today,
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    currentDate: todayStr,
+    today: util.today(new Date),//for today
+    curMonth: util.curMonth(new Date),//for today
   },
   onLoad: function () {
     var that = this
@@ -31,7 +28,8 @@ Page({
       success: function (res) {
         that.setData({
           winWidth: res.windowWidth,
-          winHeight: res.windowHeight
+          winHeight: res.windowHeight,
+          scrollViewHeight: res.windowHeight * res.pixelRatio || 667
         });
       }
 
@@ -63,7 +61,101 @@ Page({
         }
       })
     }
+
+    this.loadCalendarData()
   },
+
+  getThisMonthDays: function(year, month) {
+    return new Date(year, month, 0).getDate();
+  },
+
+  getFirstDayOfWeek: function(year, month) {
+    return new Date(Date.UTC(year, month - 1, 1)).getDay();
+  },
+
+  // 计算当月1号前空了几个格子
+  calculateEmptyGrids: function(year, month) {
+    const firstDayOfWeek = this.getFirstDayOfWeek(year, month);
+    let empytGrids = [];
+    if (firstDayOfWeek > 0) {
+      for (let i = 0; i < firstDayOfWeek; i++) {
+        empytGrids.push(i);
+      }
+      this.setData({
+        hasEmptyGrid: true,
+        empytGrids
+      });
+    } else {
+      this.setData({
+        hasEmptyGrid: false,
+        empytGrids: []
+      });
+    }
+  },
+  // 绘制当月天数占的格子
+  calculateDays: function (year, month) {
+    let days = [];
+    const thisMonthDays = this.getThisMonthDays(year, month);
+    for (let i = 1; i <= thisMonthDays; i++) {
+      days.push(i);
+    }
+    this.setData({
+      days
+    });
+  },
+
+  loadCalendarData: function() {
+    const date = new Date();
+    const cur_year = date.getFullYear();
+    const cur_month = date.getMonth() + 1;
+    const weeks_ch = ['日', '一', '二', '三', '四', '五', '六'];
+    this.calculateEmptyGrids(cur_year, cur_month);
+    this.calculateDays(cur_year, cur_month);
+    this.setData({
+      cur_year,
+      cur_month,
+      weeks_ch
+    })
+  },
+
+  handleCalendar(e) {
+    const handle = e.currentTarget.dataset.handle;
+    const cur_year = this.data.cur_year;
+    const cur_month = this.data.cur_month;
+    if (handle === 'prev') {
+      let newMonth = cur_month - 1;
+      let newYear = cur_year;
+      if (newMonth < 1) {
+        newYear = cur_year - 1;
+        newMonth = 12;
+      }
+
+      this.calculateDays(newYear, newMonth);
+      this.calculateEmptyGrids(newYear, newMonth);
+
+      this.setData({
+        cur_year: newYear,
+        cur_month: newMonth
+      })
+
+    } else {
+      let newMonth = cur_month + 1;
+      let newYear = cur_year;
+      if (newMonth > 12) {
+        newYear = cur_year + 1;
+        newMonth = 1;
+      }
+
+      this.calculateDays(newYear, newMonth);
+      this.calculateEmptyGrids(newYear, newMonth);
+
+      this.setData({
+        cur_year: newYear,
+        cur_month: newMonth
+      })
+    }
+  },
+
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
--- wxapp/utils/util.js
+++ wxapp/utils/util.js
@@ -14,6 +14,16 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+function today(date) {//显示今天的日
+  return date.getDate();
+}
+
+function curMonth(date) {//显示今天的月
+  return date.getMonth() + 1;
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  today: today,//显示今天的日
+  curMonth: curMonth//显示今天的月
 }
